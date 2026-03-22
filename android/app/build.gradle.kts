1ca317f23dc78ed13c0a844ae1fe351cc976c1b7
刘老家 <liulaoye@family.com>
2026-03-22 20:47:52 +0800

    fix: 修复Android配置，设置minSdk=21，简化依赖

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,14 +20,11 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.liu_family_library"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        applicationId = "com.liu.family.library"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
     }
 
     buildTypes {
